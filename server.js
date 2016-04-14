88c7d9ecf15c022eda2b6373cb22bf83ea9304da
Tarang Khanna <khanna17@purdue.edu>
2016-04-14 01:48:05 -0400

    server sockets on

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-var appPort = 32233;
+var serverPort = 43543;
+
+// Import
 
 var express = require('express'), app = express();
 var http = require('http')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
-
 var jade = require('jade');
-var io = require('socket.io').listen(app);
+
+// Using Jade
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -14,9 +16,28 @@ app.set("view options", { layout: false });
 
 app.use(express.static(__dirname + '/public'));
 
+// Render and send the main page
+
 app.get('/', function(req, res){
   res.render('home.jade');
 });
-server.listen(appPort);
-console.log("Server listening on port " + appPort);
+server.listen(serverPort);
+console.log("Server listening on port " + serverPort);
+
+io.sockets.on('connection', function (socket) {
+    // new connection
+	socket.on('setName', function (data) {
+		socket.set('name', data);
+	});
+
+	socket.on('message', function (data) { // Broadcast the message
+		if(pseudoSet(socket))
+		{
+			var transmit = {date : new Date().toISOString(), pseudo : socket.nickname, message : data};
+			socket.broadcast.emit('message', transmit);
+			console.log("user "+ transmit['pseudo'] +" said \""+data+"\"");
+		}
+	});
+});
+
 
