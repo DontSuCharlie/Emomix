ae89a4c458ab0c10838943bdb4d654a6041de7b3
Tarang Khanna <khanna17@purdue.edu>
2016-04-13 22:37:58 -0400

    skeletal for socket

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-var express = require('express'), app = express.createServer();
+var appPort = 32233;
+
+var express = require('express'), app = express();
+var http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
 var jade = require('jade');
+var io = require('socket.io').listen(app);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
-app.configure(function() {
-    app.use(express.static(__dirname + '/public'));
-});
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   res.render('home.jade');
 });
-app.listen(3000);
+server.listen(appPort);
+console.log("Server listening on port " + appPort);
+
