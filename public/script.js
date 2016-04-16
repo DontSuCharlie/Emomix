b6f774171e6c2863d20409b14c4d7edf92637564
Tarang Khanna <khanna17@purdue.edu>
2016-04-16 17:56:04 -0400

    check for same name
    also working on typing feature

--- public/script.js
+++ public/script.js
@@ -1,28 +1,58 @@
 // client side config
 var socket = io.connect();
+// to see if user is typing
+var typing = false;
+var timeout = undefined;
+
+function timeoutFunction(){
+  typing = false;
+  socket.emit("typing", false);
+}
+
+socket.on("isTyping", function(data) {  
+  if (data.isTyping) {
+    if ($("#"+data.person+"").length === 0) {
+      $("#updates").append("<li id='"+ data.person +"'><span class='text-muted'><small><i class='fa fa-keyboard-o'></i>" + data.person + " is typing.</small></li>");
+      timeout = setTimeout(timeoutFunction, 5000);
+    }
+  } else {
+    $("#"+data.person+"").remove();
+  }
+});
+
 function addMessage(msg, name) {
     $("#chatEntries").append('<div class="message"><p>' + name + ' : ' + msg + '</p></div>');
 }
 
-function sentMessage() {
+function sendMessage() {
     if ($('#messageInput').val() != "") 
     {
         socket.emit('message', $('#messageInput').val());
         addMessage($('#messageInput').val(), "Me"); // replace Me with current user
-        $('#messageInput').val('');
+        $('#messageInput').val(''); // clear
     }
 }
 function setName() {
     if ($("#nameInput").val() != "")
     {
-        socket.emit('setName', $("#nameInput").val());
-        $('#chatControls').show();
-        $('#nameInput').hide();
-        $('#nameSet').hide();
-        $("#welcomeParagraph").show();
-        $("#welcomeParagraph").append('<div class="Welcome"><p> Hello! Welcome ' + $("#nameInput").val() + '</p></div>');
+    	socket.emit('setName', $("#nameInput").val());
+        socket.on('nameStatus', function(data){
+			if(data == "ok")
+			{
+		        $('#chatControls').show();
+		        $('#nameInput').hide();
+		        $('#nameSet').hide();
+		        $("#welcomeParagraph").show();
+		        $("#welcomeParagraph").append('<div class="Welcome"><p> Hello! Welcome ' + $("#nameInput").val() + '</p></div>');
+			}
+			else
+			{
+				alert("Name Already Taken");
+			}
+		})
     }
 }
+
 socket.on('message', function(data) {
     addMessage(data['message'], data['name']);
     console.log(data);
@@ -32,7 +62,20 @@ socket.on('message', function(data) {
 
 $(function() {
     $("#chatControls").hide();
+    $("#messageInput").keypress(function(e) {
+		// alert("typing");
+	 	 if (e.which !== 13) {
+		    if (typing === false && $("#messageInput").is(":focus")) {
+		      typing = true;
+		      socket.emit("typing", true);
+		    } else {
+		      clearTimeout(timeout);
+		      timeout = setTimeout(timeoutFunction, 5000);
+		    }
+		}
+	});
+
     $("#nameSet").click(function() {setName()});
-    $("#submit").click(function() {sentMessage();});
+    $("#submit").click(function() {sendMessage();});
     $("#welcomeParagraph").hide();
 });
