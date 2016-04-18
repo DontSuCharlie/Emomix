7d0acc60424cb4fd6f380384f46f3a116f63e6e1
Tarang Khanna <khanna17@purdue.edu>
2016-04-18 12:29:23 -0400

    added push notifications

--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
-
 // client side config
 var socket = io.connect();
 // to see if user is typing
@@ -63,7 +62,45 @@ function setName() {
 socket.on('message', function(data) {
     addMessage(data['message'], data['name']);
     console.log(data);
+    notifyMe(data['name'],data['message']);
 });
+
+// push notifications 
+function notifyMe(user,message) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+  // Let's check if the user is okay to get some notification
+  else if (Notification.permission === "granted") {
+    // If okay let's create a notification
+  var options = {
+        body: message,
+        dir : "ltr"
+    };
+  var notification = new Notification(user + " Sent a message",options);
+  }
+  // Otherwise, we need to ask the user for permission
+  // Note, Chrome does not implement the permission static property
+  // So we have to check for NOT 'denied' instead of 'default'
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // Whatever the user answers, we make sure we store the information
+      if (!('permission' in Notification)) {
+        Notification.permission = permission;
+      }
+      // If the user is okay, let's create a notification
+      if (permission === "granted") {
+        var options = {
+                body: message,
+                dir : "ltr"
+        };
+        var notification = new Notification(user + " Sent a message",options);
+      }
+    });
+  }
+  // Do nothing if user denies notification
+}
 
 // init
 
